193778a4ae2df771f5260afed84463dbfbf58e27
Douglas Kunzler <do89161@gmail.com>
2024-01-22 17:24:38 -0300

    Atualização

--- contact/admin.py
+++ contact/admin.py
@@ -4,13 +4,14 @@ from contact import models
 # Register your models here.
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id', 'first_name', 'last_name', 'phone',
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'show',
     ordering = 'id',
     # list_filter = 'created_date', --> Cria um filtro
     search_fields = 'id', 'first_name', 
     list_per_page = 10
     list_max_show_all = 50
-    list_display_links = 'id', 'first_name'
+    list_editable = 'show',
+    list_display_links = 'id', 'first_name',
 
 @admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
---
+++ contact/views/contact_views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, get_list_or_404
+from contact.models import Contact
+from django.http import Http404
+
+
+def index(request):
+    contacts = Contact.objects \
+    .filter(show=True)\
+    .order_by('-id')[0:10]
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Contatos - ',
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context,
+    )
+
+def contact(request, contact_id):
+    single_contact = get_list_or_404(Contact, pk=contact_id, show=True)
+
+    # single_contact = Contact.objects.filter(pk=contact_id).first()
+
+    # if single_contact is None:
+    #     raise Http404()
+
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
+
+    context = {
+        'contact': single_contact,
+        'site_title': site_title,
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
+        context,
+    )
