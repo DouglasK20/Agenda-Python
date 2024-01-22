cf0cc326c4e72268b9e69d2cab4db6bb4e21f18d
Douglas Kunzler <do89161@gmail.com>
2024-01-22 11:37:26 -0300

    Criei uma categoria

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from contact import models
 
 # Register your models here.
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id', 'first_name', 'last_name', 'phone',
+    ordering = 'id',
+    # list_filter = 'created_date', --> Cria um filtro
+    search_fields = 'id', 'first_name', 
+    list_per_page = 10
+    list_max_show_all = 50
+    list_display_links = 'id', 'first_name'
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = 'id',
