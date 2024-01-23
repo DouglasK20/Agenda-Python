303932159b5ec98a50154578dc24d84dd02724da
Douglas Kunzler <do89161@gmail.com>
2024-01-23 17:21:07 -0300

    Atualização

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,15 +1,19 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
 from contact.models import Contact
-from django.http import Http404
+from django.db.models import Q
 
 
-def index(request):
+def index(request):    
     contacts = Contact.objects \
-    .filter(show=True)\
-    .order_by('-id')[0:10]
+        .filter(show=True)\
+        .order_by('-id')
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - ',
     }
 
@@ -19,8 +23,41 @@ def index(request):
         context,
     )
 
+def search(request):
+
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+    
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+        )\
+        .order_by('id')
+    
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'page_obj': page_obj,
+        'site_title': 'Search - ',
+        'search_value': search_value,
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context,
+    )
+
 def contact(request, contact_id):
-    single_contact = get_list_or_404(Contact, pk=contact_id, show=True)
+    single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
 
     # single_contact = Contact.objects.filter(pk=contact_id).first()
 
@@ -31,11 +68,11 @@ def contact(request, contact_id):
 
     context = {
         'contact': single_contact,
-        'site_title': site_title,
+        'site_title': site_title
     }
 
     return render(
         request,
         'contact/contact.html',
-        context,
+        context
     )
